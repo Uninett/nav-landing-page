b5508312f8407964f6e32531b849049f0f82c3e1
Christian Strand Young <christian@strandyoung.com>
2014-09-16 13:20:57 +0200

    Upgraded to Django 1.7
    --HG--
    branch : dynamicreleases

--- nav_landing_page/settings.py
+++ nav_landing_page/settings.py
@@ -117,7 +117,6 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'south',
     'chunks',
     'markdown_deux',
     'releases',
