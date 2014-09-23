0d8237341fcbef5b229713b623672f5803ac5663
Christian Strand Young <christian@strandyoung.com>
2014-09-23 15:57:58 +0200

    Removed home view context, redundant with new context processor
    --HG--
    branch : dynamicreleases

--- nav_landing_page/views.py
+++ nav_landing_page/views.py
@@ -2,8 +2,7 @@ from django.shortcuts import render
 from releases.models import Release
 
 def home(request):
-    latest_stable_version = Release.latest_stable_version()
-    return render(request, 'index.html', {'latest_stable_version' : latest_stable_version})
+    return render(request, 'index.html')
 
 
 def install_instructions(request):
