85884452c22432b86007afff29e2557d4780e898
Morten Brekkevold <morten.brekkevold@uninett.no>
2014-11-12 10:28:31 +0100

    Use environment to determine whether to run in debug
    --HG--
    branch : dynamicreleases

--- nav_landing_page/settings.py
+++ nav_landing_page/settings.py
@@ -22,7 +22,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = 'wd#p5+pca*5&@sgz(38ef9ku8vte^^t#lq*x#za3h0251@)@ds'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get(
+    'DJANGO_DEBUG', '0').lower() in ('1', 'true', 'on', 'yes')
 
 TEMPLATE_DEBUG = True
 
