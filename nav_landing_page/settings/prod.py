17a548eb3f77abcee3c0f9eb089059dbcaf4bc85
Christian Strand Young <christian@strandyoung.com>
2015-04-10 15:12:44 +0200

    Make secret key fail if not replaced in production
    --HG--
    branch : dynamicreleases

--- nav_landing_page/settings/prod.py
+++ nav_landing_page/settings/prod.py
@@ -1,7 +1,7 @@
 import os
 
 # Replaced by docker script on deploy
-SECRET_KEY = 'wd#p5+pca*5&@sgz(38ef9ku8vte^^t#lq*x#za3h0251@)@ds'
+SECRET_KEY = ''
 
 DEBUG = os.environ.get('DJANGO_DEBUG', '0').lower() in ('1', 'true', 'on', 'yes')
 TEMPLATE_DEBUG = DEBUG
