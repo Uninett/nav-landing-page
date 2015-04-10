4af2dbfad1c20b08136987cbf8e4e1107c6d58dc
Christian Strand Young <christian@strandyoung.com>
2015-04-10 15:12:23 +0200

    Dummy secret key in local
    --HG--
    branch : dynamicreleases

--- nav_landing_page/settings/local.py
+++ nav_landing_page/settings/local.py
@@ -1,5 +1,5 @@
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'wd#p5+pca*5&@sgz(38ef9ku8vte^^t#lq*x#za3h0251@)@ds'
+SECRET_KEY = 'change-this-in-production'
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG 
