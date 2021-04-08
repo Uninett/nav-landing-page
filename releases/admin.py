b83443821f3e76dba64cd5c5740b6ed6b7638999
Morten Brekkevold <morten.brekkevold@uninett.no>
2021-04-08 12:23:13 +0200

    Delete all code from the Django app
    Because:
    - The site is transitioning to a static site generator. All of the
      Python code becomes obsolete.

--- releases/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from releases.models import Release
-
-# Register your models here.
-class ReleasesAdmin(admin.ModelAdmin):
-	model = Release
-
-admin.site.register(Release, ReleasesAdmin)
